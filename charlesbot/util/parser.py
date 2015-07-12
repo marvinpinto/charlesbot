65c98e926ea60145d2616041c18b7b4722e6d98f
Marvin Pinto <marvin@pinto.im>
2015-07-12 17:38:04 -0400

    Silence the expected nosetests output when testing with bogus input

--- charlesbot/util/parser.py
+++ charlesbot/util/parser.py
@@ -1,4 +1,7 @@
 import argparse
+import sys
+from contextlib import contextmanager
+from io import StringIO
 
 
 def parse_args(args):
@@ -14,3 +17,14 @@ def parse_args(args):
     )
 
     return parser.parse_args(args)
+
+
+@contextmanager
+def capture_sys_output():
+    caputure_out, capture_err = StringIO(), StringIO()
+    current_out, current_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = caputure_out, capture_err
+        yield caputure_out, capture_err
+    finally:
+        sys.stdout, sys.stderr = current_out, current_err
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,12 +1,14 @@
 import unittest
 from charlesbot.util.parser import parse_args
+from charlesbot.util.parser import capture_sys_output
 
 
 class TestParser(unittest.TestCase):
 
     def test_empty_config(self):
         with self.assertRaises(SystemExit) as cm:
-            parse_args([])
+            with capture_sys_output() as (stdout, stderr):
+                parse_args([])
         self.assertEqual(cm.exception.code, 2)
 
     def test_config_supplied_1(self):
@@ -27,15 +29,18 @@ class TestParser(unittest.TestCase):
 
     def test_config_invalid_1(self):
         with self.assertRaises(SystemExit) as cm:
-            parse_args(['--config'])
+            with capture_sys_output() as (stdout, stderr):
+                parse_args(['--config'])
         self.assertEqual(cm.exception.code, 2)
 
     def test_config_invalid_2(self):
         with self.assertRaises(SystemExit) as cm:
-            parse_args(['-c'])
+            with capture_sys_output() as (stdout, stderr):
+                parse_args(['-c'])
         self.assertEqual(cm.exception.code, 2)
 
     def test_bogus_args(self):
         with self.assertRaises(SystemExit) as cm:
-            parse_args(['--bogus'])
+            with capture_sys_output() as (stdout, stderr):
+                parse_args(['--bogus'])
         self.assertEqual(cm.exception.code, 2)
