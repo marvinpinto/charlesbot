aafff01619dfb5ffdb16c1fb1c17a2a435513e0a
Marvin Pinto <marvin@pinto.im>
2015-08-01 18:09:46 -0400

    Automatically deploy charlesbot to PyPI whenever a new tag is created

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
 
+long_description = """
+Full details available at https://github.com/marvinpinto/charlesbot
+"""
+
 setup(
     name='charlesbot',
     version='0.1.0',
+    description='Slack Real Time Messaging robot written in Python 3!',
+    long_description=long_description,
     author='Marvin Pinto',
     author_email='marvin@pinto.im',
+    license='MIT',
     url="https://github.com/marvinpinto/charlesbot",
     packages=['charlesbot'],
     entry_points={
