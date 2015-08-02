dc993c85f1c14ef2bfa44d402dd51b6b9ef60b0e
Marvin Pinto <marvin@pinto.im>
2015-08-02 12:11:18 -0400

    Make sure that setup.py refers to all the charlesbot sub-packages
    The problem I ran into here was that `pip install charlesbot` did not
    bring in anything under the top `charlesbot` namespace -- so it was
    missing `charlesbot.util`, etc.
    
    While I was here I added a bit more metadata to setup.py and to make
    sure it brings in its dependencies and that sort of thing.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 long_description = """
 Full details available at https://github.com/marvinpinto/charlesbot
@@ -6,19 +6,33 @@ Full details available at https://github.com/marvinpinto/charlesbot
 
 setup(
     name='charlesbot',
-    version='0.1.0',
+    version='0.1.1',
     description='Slack Real Time Messaging robot written in Python 3!',
     long_description=long_description,
     author='Marvin Pinto',
     author_email='marvin@pinto.im',
     license='MIT',
+
+   # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='slack robot chatops automation',
+
     url="https://github.com/marvinpinto/charlesbot",
-    packages=['charlesbot'],
+    packages=find_packages(exclude=['images', 'tests*']),
+
     entry_points={
-        'console_scripts': [
-            'charlesbot = charlesbot.__main__:main'
-            ]
+        'console_scripts': ['charlesbot = charlesbot.__main__:main']
     },
-    install_requires=[],
+
+    install_requires=[
+        "slackclient",
+        "websocket-client"
+    ],
+
     test_suite='nose.collector'
 )
