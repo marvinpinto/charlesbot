190ab3942fb0d164a76d4da35c3d87cf7408c9ac
Marvin Pinto <marvin@pinto.im>
2015-08-03 14:40:43 -0400

    Abstract out the the plugin functionality that will be common across all plugins into its own abstract base class

---
+++ charlesbot/base_plugin.py
@@ -0,0 +1,42 @@
+from abc import ABCMeta, abstractmethod
+import asyncio
+import logging
+
+
+class BasePlugin(metaclass=ABCMeta):
+
+    def __init__(self, slack_client, plugin_name):
+        self.set_running(True)
+        self.log = logging.getLogger(__name__)
+        self.sc = slack_client
+        self._plugin_name = plugin_name
+        self.log.info("Initializing the %s plugin" % plugin_name)
+        self._q = asyncio.Queue()
+        self.initialize_queue_consumer()
+
+    def initialize_queue_consumer(self):
+        loop = asyncio.get_event_loop()
+        loop.create_task(self.consume())
+
+    @asyncio.coroutine
+    def consume(self):
+        while self.is_running():
+            value = yield from self._q.get()
+            yield from self.process_message(value)
+
+    def get_plugin_name(self):
+        return self._plugin_name
+
+    def is_running(self):
+        return self._is_running
+
+    def set_running(self, running):
+        self._is_running = running
+
+    @asyncio.coroutine
+    def queue_message(self, message):
+        yield from self._q.put(message)
+
+    @abstractmethod
+    def process_message(self, messages):
+        pass
---
+++ tests/test_base_plugin.py
@@ -0,0 +1,69 @@
+import asynctest
+import asyncio
+from asynctest.mock import MagicMock
+from charlesbot.base_plugin import BasePlugin
+
+
+class TestBasePlugin(asynctest.TestCase):
+
+    class DummyPlugin(BasePlugin):
+        def __init__(myself, slack_client):
+            super().__init__(slack_client, "Dummy")
+            myself.call_counter = 0
+
+        @asyncio.coroutine
+        def process_message(myself, message):
+            myself.call_counter += 1
+
+    def setUp(self):
+        self.slack_client = MagicMock()
+        self.initialize_dummy_plugin()
+
+    def tearDown(self):
+        self.initialize_dummy_plugin()
+
+    def initialize_dummy_plugin(self):
+        self.dc = TestBasePlugin.DummyPlugin(self.slack_client)
+        self.dc.set_running(False)
+
+    @asynctest.ignore_loop
+    def test_get_plugin_name(self):
+        self.assertEqual(self.dc.get_plugin_name(), "Dummy")
+        self.dc._plugin_name = "plug2"
+        self.assertEqual(self.dc.get_plugin_name(), "plug2")
+        self.dc._plugin_name = ""
+        self.assertEqual(self.dc.get_plugin_name(), "")
+        self.dc._plugin_name = None
+        self.assertEqual(self.dc.get_plugin_name(), None)
+
+    @asynctest.ignore_loop
+    def test_plugin_running(self):
+        self.assertEqual(self.dc.is_running(), False)
+        self.dc.set_running(True)
+        self.assertEqual(self.dc.is_running(), True)
+
+    def test_queue_message(self):
+        yield from self.dc.queue_message("msg one")
+        self.assertEqual(self.dc._q.qsize(), 1)
+        q_val = yield from self.dc._q.get()
+        self.assertEqual(q_val, "msg one")
+        yield from self.dc.queue_message("msg two")
+        yield from self.dc.queue_message("msg three")
+        self.assertEqual(self.dc._q.qsize(), 2)
+        q_val = yield from self.dc._q.get()
+        self.assertEqual(q_val, "msg two")
+        q_val = yield from self.dc._q.get()
+        self.assertEqual(q_val, "msg three")
+        self.assertTrue(self.dc._q.empty())
+        self.assertEqual(self.dc._q.qsize(), 0)
+
+    def test_queue_consume(self):
+        self.assertEqual(self.dc.call_counter, 0)
+        self.dc.is_running = MagicMock()
+        self.dc.is_running.side_effect = [True, True, False]
+        yield from self.dc.queue_message("one")
+        yield from self.dc.queue_message("two")
+        yield from self.dc.queue_message("three")
+        yield from self.dc.consume()
+        self.assertEqual(self.dc._q.qsize(), 1)
+        self.assertEqual(self.dc.call_counter, 2)
