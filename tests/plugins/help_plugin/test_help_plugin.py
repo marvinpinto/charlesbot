2e67773e17917d0acaeaaeebca5becff5ad03a1e
Marvin Pinto <marvin@pinto.im>
2015-08-16 17:53:55 -0400

    Refactor the Help plugin to use the new Slack objects

--- tests/plugins/help_plugin/test_help_plugin.py
+++ tests/plugins/help_plugin/test_help_plugin.py
@@ -3,6 +3,7 @@ import re
 from asynctest.mock import MagicMock
 from asynctest.mock import call
 from charlesbot.plugins.help_plugin import Help
+from charlesbot.slack.slack_message import SlackMessage
 
 
 class TestHelpPlugin(asynctest.TestCase):
@@ -16,7 +17,7 @@ class TestHelpPlugin(asynctest.TestCase):
 
     def initialize_help_plugin(self):
         self.hp = Help(self.slack_client)
-        self.hp.handle_single_prefixed_message = MagicMock()
+        self.hp.send_help_message = MagicMock()
 
     @asynctest.ignore_loop
     def test_help_message_wrapper_str(self):
@@ -26,59 +27,48 @@ class TestHelpPlugin(asynctest.TestCase):
         m = p.match(help_str)
         self.assertTrue(m)
 
+    def test_wrong_message_type(self):
+        msg = ""
+        yield from self.hp.process_message(msg)
+        self.hp.send_help_message.assert_has_calls([])
+
     def test_two_msgs_no_good(self):
-        messages = [
-            {
-                "type": "message",
-                "user": "U2147483697",
-                "channel": "C2147483705",
-                "text": "This is not me asking for prefix",
-            },
-            {
-                "type": "message",
-                "user": "U2147483697",
-                "channel": "C2147483705",
-                "text": "This is not me asking for help, either",
-            }
-        ]
-        yield from self.hp.process_message(messages)
-        self.hp.handle_single_prefixed_message.assert_has_calls([])
+        msg = SlackMessage(type="message",
+                           user="U2147483697",
+                           channel="C2147483705",
+                           text="This is not me asking for prefix")
+        yield from self.hp.process_message(msg)
+        msg = SlackMessage(type="message",
+                           user="U2147483697",
+                           channel="C2147483705",
+                           text="This is not me asking for help, either")
+        yield from self.hp.process_message(msg)
+        self.hp.send_help_message.assert_has_calls([])
 
     def test_two_msgs_one_good(self):
-        messages = [
-            {
-                "type": "message",
-                "user": "U2147483697",
-                "channel": "C1",
-                "text": "!help fine, now I need help",
-            },
-            {
-                "type": "message",
-                "user": "U2147483697",
-                "channel": "C2",
-                "text": "This is not me asking for help, either",
-            }
-        ]
-        yield from self.hp.process_message(messages)
+        msg = SlackMessage(type="message",
+                           user="U2147483697",
+                           channel="C1",
+                           text="!help fine, now I need help")
+        yield from self.hp.process_message(msg)
+        msg = SlackMessage(type="message",
+                           user="U2147483697",
+                           channel="C2147483705",
+                           text="This is not me asking for help, either")
+        yield from self.hp.process_message(msg)
         expected = [call('C1')]
-        self.hp.handle_single_prefixed_message.assert_has_calls(expected)
+        self.hp.send_help_message.assert_has_calls(expected)
 
     def test_two_msgs_two_good(self):
-        messages = [
-            {
-                "type": "message",
-                "user": "U2147483697",
-                "channel": "C1",
-                "text": "!help fine, now I need help",
-            },
-            {
-                "type": "message",
-                "user": "U2147483697",
-                "channel": "C2",
-                "text": "!help halp me, plz",
-            }
-        ]
-        yield from self.hp.process_message(messages)
+        msg = SlackMessage(type="message",
+                           user="U2147483697",
+                           channel="C1",
+                           text="!help fine, now I need help")
+        yield from self.hp.process_message(msg)
+        msg = SlackMessage(type="message",
+                           user="U2147483697",
+                           channel="C2",
+                           text="!help Yep, still need help")
+        yield from self.hp.process_message(msg)
         expected = [call('C1'), call('C2')]
-        self.hp.handle_single_prefixed_message.assert_has_calls(expected,
-                                                                any_order=True)
+        self.hp.send_help_message.assert_has_calls(expected, any_order=True)
